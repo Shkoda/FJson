ee7d9a8663d01636c7ced713ce802da13502928b
Shkoda <shkoshka@gmail.com>
2017-07-03 03:40:27 +0300

    smth

---
+++ Tutorial1/Tutorial1/JsonSchema.fs
@@ -0,0 +1,48 @@
+﻿namespace Shkoda.Telegram.Client
+module Json  =
+    open FSharp.Data
+    open System
+
+    type JsonSchema = JsonProvider<"../data/schema.json">
+    
+    let loadJson = JsonSchema.Load("../data/schema.json")
+
+    type ParamType = 
+    | Int 
+    | Long 
+    | String 
+    | Bytes 
+    | Vector of ParamType
+    | TypeName of string
+
+    type ConstructorParam = {name:string; paramType:ParamType} with
+        static member ParseType (t:string) =
+            let VectorType (pt:string) =
+                pt.Replace("Vector<", "").Replace(">", "") |> ConstructorParam.ParseType               
+                
+            match t with
+            | "int" -> ParamType.Int
+            | "long" -> ParamType.Long
+            | "string" -> ParamType.String
+            | "bytes" -> ParamType.Bytes
+            | txt when txt.StartsWith("Vector") -> ParamType.Vector (VectorType txt)
+            | _ -> ParamType.TypeName t
+         static member Parse (n: string)(t:string) = {name = n; paramType = ConstructorParam.ParseType t}
+
+
+    type ConstructorDefinition = {id:int; predicate:string; typeName:string; layer:int; constructorParams:ConstructorParam[];parent:ConstructorDefinition option}
+
+    let constructors = 
+        loadJson.Constructors 
+        |> Seq.map (fun (raw) -> 
+            {id = raw.Id;
+            predicate = raw.Predicate;
+            typeName = raw.Type;
+            layer = raw.Layer;constructorParams = raw.Params |> Seq.map (fun p -> ConstructorParam.Parse p.Name p.Type) |> Seq.toArray; 
+            parent = None})
+       
+
+      
+
+    let a = constructors
+
---
+++ Tutorial1/Tutorial1/Main.fs
@@ -0,0 +1,11 @@
+﻿namespace Shkoda.Telegram.Client
+module Main=
+    open System
+
+    [<EntryPoint>]
+    let main args =
+        Console.WriteLine("hello")
+        let a = Json.constructors 
+
+        while true do Console.ReadLine()
+        0
